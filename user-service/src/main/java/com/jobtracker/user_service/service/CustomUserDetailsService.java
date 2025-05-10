b0914bd1f0fa975392b1175e9aeffd327fb8d98c
Bharpur Singh <honey92277@gmail.com>
2025-05-10 03:00:53 -0400

    Custom User Details Service added

---
+++ user-service/src/main/java/com/jobtracker/user_service/service/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.jobtracker.user_service.service;
+
+import com.jobtracker.user_service.model.User;
+import com.jobtracker.user_service.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(usernameOrEmail)
+                .or(() -> userRepository.findByEmail(usernameOrEmail))
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .authorities(user.getRoles().split(","))
+                .build();
+    }
+}
