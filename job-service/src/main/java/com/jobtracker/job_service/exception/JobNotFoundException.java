b1c5c66d854e675e9dbb8f1f4e395370a6b63f9f
Bharpur Singh <honey92277@gmail.com>
2025-09-24 02:37:11 -0400

    Global exception class added

--- job-service/src/main/java/com/jobtracker/job_service/exception/JobNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jobtracker.job_service.exception;
-
-public class JobNotFoundException extends RuntimeException {
-    public JobNotFoundException(Long jobId) {
-        super("Job not found with id: " + jobId);
-    }
-}
